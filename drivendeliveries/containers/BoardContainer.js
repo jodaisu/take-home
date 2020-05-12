1a212376db275c6b4db477c310d0b04d72c7905b
Joseph Renolayan <jrenolayan@gmail.com>
2020-05-12 12:53:27 -0700

    added button and input tags to change shape and colors

--- drivendeliveries/containers/BoardContainer.js
+++ drivendeliveries/containers/BoardContainer.js
@@ -4,15 +4,26 @@ import Board from "../components/Board";
 const BoardContainer = () => {
   const [inputCount, setInputCount] = useState(8);
   const [submitCount, setSubmitCount] = useState(inputCount);
+  const [shape, setShape] = useState("circle");
+  const [color, setColor] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubmitCount(inputCount);
   };
 
+  const changeShape = (e) => {
+    setShape(e);
+  };
+  const changeColor = (e) => {
+    setColor(e);
+  };
+
   return (
     <div className="container">
-      {submitCount ? <Board cellCount={submitCount} /> : null}
+      {submitCount ? (
+        <Board cellCount={submitCount} color={color} shape={shape} />
+      ) : null}
       <br />
       <form onSubmit={handleSubmit}>
         <label>
@@ -25,6 +36,39 @@ const BoardContainer = () => {
         </label>
         <input type="submit" value="Go" />
         <br />
+        <span>Shapes:</span>{" "}
+        <input
+          type="radio"
+          name="shape"
+          value="circle"
+          defaultChecked
+          onClick={(e) => changeShape(e.target.value)}
+        ></input>{" "}
+        <span>Circle</span>{" "}
+        <input
+          type="radio"
+          name="shape"
+          value="square"
+          onClick={(e) => changeShape(e.target.value)}
+        ></input>{" "}
+        <span>Square</span>
+        <br />
+        <span>Colors:</span>{" "}
+        <input
+          type="radio"
+          name="color"
+          value="1"
+          defaultChecked
+          onChange={(e) => changeColor(e.target.value)}
+        ></input>{" "}
+        <span>1</span>{" "}
+        <input
+          type="radio"
+          name="color"
+          value="2"
+          onChange={(e) => changeColor(e.target.value)}
+        ></input>{" "}
+        <span>2</span>
       </form>
     </div>
   );
