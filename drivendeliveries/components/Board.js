e01fc3e41f39e3382bae5cf6a11740927d6e0400
Joseph Renolayan <jrenolayan@gmail.com>
2020-05-12 12:14:50 -0700

    row component keeps track of which is even or odd for tiles

--- drivendeliveries/components/Board.js
+++ drivendeliveries/components/Board.js
@@ -1,4 +1,18 @@
+import Row from "./Row";
+
 const Board = ({ cellCount }) => {
-  return <div className="Board">{cellCount}</div>;
+  // loop over cellcount to render Rows
+  let rows = [];
+  for (let i = 0; i < cellCount; i++) {
+    rows.push(
+      <Row
+        key={`row${i}`}
+        row={i}
+        cellCount={cellCount}
+        evenOrOdd={i % 2 === 0 ? "even" : "odd"}
+      ></Row>
+    );
+  }
+  return <div className="Board">{rows}</div>;
 };
 export default Board;
---
+++ drivendeliveries/components/Row.js
@@ -0,0 +1,7 @@
+const Row = ({ cellCount, row, evenOrOdd }) => {
+  let squares = [];
+
+  return <div className="row">{evenOrOdd}</div>;
+};
+
+export default Row;
